ab63161972bde16bbffa895253861fb5fdc90068
LeechanX <leechan8@outlook.com>
2015-11-03 19:00:09 +0800

    20151103

---
+++ Longest Increasing Subsequence.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int lengthOfLIS(int* nums, int numsSize) {
+	if (!numsSize) {
+		return 0;
+	}
+    int m[numsSize], i, j;
+    int maxLength = 1;
+    for (i = 0;i < numsSize; ++i) {
+    	m[i] = 1;
+    }
+    for (i = 1;i < numsSize; ++i) {
+    	for (j = 0;j < i; ++j) {
+    		if (nums[j] < nums[i] && m[j] + 1 > m[i]) {
+    			m[i] = m[j] + 1;
+    		}
+    	}
+    	if (m[i] > maxLength) {
+    		maxLength = m[i];
+    	}
+    }
+    return maxLength;
+}
+
+int main() {
+	int data[] = {10, 9, 2, 5, 3, 7, 101, 18};
+	int size = sizeof data / sizeof *data;
+	printf("%d\n", lengthOfLIS(data, size));
+}
