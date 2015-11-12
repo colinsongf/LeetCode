7298d84065ec45d6dfba9a2b4c523282acf69437
LeechanX <leechan8@outlook.com>
2015-11-12 11:05:31 +0800

    20151112up

--- Range Sum Query - Immutable.cpp
+++ Range Sum Query - Immutable.cpp
@@ -1,10 +1,7 @@
 class NumArray {
 public:
     NumArray(vector<int> &nums) : subSummary(nums.size()) {
-        if (nums.size() == 0) {
-            available = false;
-        } else {
-            available = true;
+        if (nums.size() != 0) {
             subSummary[0] = nums[0];
             for (int i = 1;i < nums.size(); ++i) {
                 subSummary[i] = subSummary[i - 1] + nums[i];
@@ -13,12 +10,11 @@ public:
     }
 
     int sumRange(int i, int j) {
-        if (available)
+        if (subSummary.size() != 0)
             return i > 0 ? subSummary[j] - subSummary[i - 1] : subSummary[j];
         else
             return 0;
     }
 private:
     vector<int> subSummary;
-    bool available;
 };
