63386ca0c8a2c5ee5c60f789b375b5dbffe6e3df
LeechanX <leechan8@outlook.com>
2016-02-18 11:14:32 +0800

    20150218

---
+++ Reconstruct Itinerary.py
@@ -0,0 +1,32 @@
+class Solution:
+    def backtrace(self, graph, end, step, currentnode, subanswer):
+        if self.answer:return
+        if step == end:
+            self.answer = subanswer[:]
+            return
+        if currentnode not in graph:return
+        neibors = graph[currentnode]
+        for neibor in sorted(neibors):
+            subanswer.append(neibor)
+            graph[currentnode].remove(neibor)
+            self.backtrace(graph, end, step + 1, neibor, subanswer)
+            subanswer.pop()
+            graph[currentnode].append(neibor)
+
+    def findItinerary(self, tickets):
+        graph = {}
+        self.answer = None
+        for fromto in tickets:
+            start, end = fromto[0], fromto[1]
+            if start not in graph:
+                graph[start] = []
+            graph[start].append(end)
+        edges = len(tickets)
+        subanswer = ['JFK']
+        self.backtrace(graph, edges, 0, 'JFK', subanswer)
+        return self.answer
+
+ins = Solution()
+tickets = [["AXA","EZE"],["EZE","AUA"],["ADL","JFK"],["ADL","TIA"],["AUA","AXA"],["EZE","TIA"],["EZE","TIA"],["AXA","EZE"],["EZE","ADL"],["ANU","EZE"],["TIA","EZE"],["JFK","ADL"],["AUA","JFK"],["JFK","EZE"],["EZE","ANU"],["ADL","AUA"],["ANU","AXA"],["AXA","ADL"],["AUA","JFK"],["EZE","ADL"],["ANU","TIA"],["AUA","JFK"],["TIA","JFK"],["EZE","AUA"],["AXA","EZE"],["AUA","ANU"],["ADL","AXA"],["EZE","ADL"],["AUA","ANU"],["AXA","EZE"],["TIA","AUA"],["AXA","EZE"],["AUA","SYD"],["ADL","JFK"],["EZE","AUA"],["ADL","ANU"],["AUA","TIA"],["ADL","EZE"],["TIA","JFK"],["AXA","ANU"],["JFK","AXA"],["JFK","ADL"],["ADL","EZE"],["AXA","TIA"],["JFK","AUA"],["ADL","EZE"],["JFK","ADL"],["ADL","AXA"],["TIA","AUA"],["AXA","JFK"],["ADL","AUA"],["TIA","JFK"],["JFK","ADL"],["JFK","ADL"],["ANU","AXA"],["TIA","AXA"],["EZE","JFK"],["EZE","AXA"],["ADL","TIA"],["JFK","AUA"],["TIA","EZE"],["EZE","ADL"],["JFK","ANU"],["TIA","AUA"],["EZE","ADL"],["ADL","JFK"],["ANU","AXA"],["AUA","AXA"],["ANU","EZE"],["ADL","AXA"],["ANU","AXA"],["TIA","ADL"],["JFK","ADL"],["JFK","TIA"],["AUA","ADL"],["AUA","TIA"],["TIA","JFK"],["EZE","JFK"],["AUA","ADL"],["ADL","AUA"],["EZE","ANU"],["ADL","ANU"],["AUA","AXA"],["AXA","TIA"],["AXA","TIA"],["ADL","AXA"],["EZE","AXA"],["AXA","JFK"],["JFK","AUA"],["ANU","ADL"],["AXA","TIA"],["ANU","AUA"],["JFK","EZE"],["AXA","ADL"],["TIA","EZE"],["JFK","AXA"],["AXA","ADL"],["EZE","AUA"],["AXA","ANU"],["ADL","EZE"],["AUA","EZE"]]
+
+print ins.findItinerary(tickets)
